463d8b4a6c8d5b54246d1ba9e2f61d5ea0f261d6
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-03-08 18:45:04 +0700

    Add not found page

--- front-end/src/pages/NotFound.vue
+++ front-end/src/pages/NotFound.vue
@@ -1,3 +1,19 @@
 <template>
-  Not Found
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import BaseCard from '@/components/ui/BaseCard.vue';
+
+export default {
+  components: {BaseCard}
+};
+</script>
