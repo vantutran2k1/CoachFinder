9ca1a2f307fd829827ec316af2673b87112628ff
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-03-11 23:00:39 +0700

    Create async components for better UX

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,13 +1,13 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import CoachesList from '@/pages/coaches/CoachesList.vue';
-import CoachDetail from '@/pages/coaches/CoachDetail.vue';
-import ContactCoach from '@/pages/requests/ContactCoach.vue';
-import CoachRegistration from '@/pages/coaches/CoachRegistration.vue';
-import RequestsReceived from '@/pages/requests/RequestsReceived.vue';
 import NotFound from '@/pages/NotFound.vue';
-import UserAuth from '@/pages/auth/UserAuth.vue';
 import store from '@/store';
 
+const CoachDetail = () => import('@/pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('@/pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('@/pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('@/pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('@/pages/auth/UserAuth.vue');
 
 const router = createRouter({
 	history: createWebHistory(),
